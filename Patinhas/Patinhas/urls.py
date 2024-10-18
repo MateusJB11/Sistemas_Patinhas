01d7ff0b09f765471dbedda7937af3fbd0f4cfb3
M&M <silva.mateus159@gmail.com>
2024-10-18 17:51:07 -0300

    Começando o HTML e corrigindo alguns erros

--- Patinhas/Patinhas/urls.py
+++ Patinhas/Patinhas/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from aplic.views import IndexView
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
 ]
